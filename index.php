e727561bbc833c66216d701d288bed6b15fc5cdf
pz <keith837@sohu.com>
2017-01-24 22:03:44 +0800

    aaaa

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Some rights reserved：www.thinkcmf.comsshghgh
  */
 if (ini_get('magic_quotes_gpc')) {
-    $a=123;
+    $a=12312;
 	function stripslashesRecursive(array $array){
 		foreach ($array as $k => $v) {
 			if (is_string($v)){
